923c64bd5b4f944e1d4dd5e79a91fd84edb2f6e2
bpaulsen0 <benjaminpaulsen0@gmail.com>
2022-08-20 10:54:24 -0500

    trying to fix that stupid error

--- pages/lodging.js
+++ pages/lodging.js
@@ -8,6 +8,9 @@ export default function Tour() {
     <div className={styles.content}>
             <a className={styles.back} href='/'>⊲Back to Homepage</a>
             <h1 className={styles.title}>The Lodging</h1>
+            <a className={styles.point} href='https://www.wyndhamhotels.com/americinn/ashland-wisconsin/americinn-ashland/overview?CID=LC:AA::GGL:RIO:National:52038&iata=00093796' target='_blank' rel='noopener noreferrer'>AmericInn</a>
         </div>
   )
 }
+
+//https://www.wyndhamhotels.com/americinn/ashland-wisconsin/americinn-ashland/overview?CID=LC:AA::GGL:RIO:National:52038&iata=00093796
