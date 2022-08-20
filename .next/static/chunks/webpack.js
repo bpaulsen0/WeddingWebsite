9c1acda726224364329c9c9b178e4cf5fd9e6668
bpaulsen0 <benjaminpaulsen0@gmail.com>
2022-08-20 10:39:23 -0500

    tour urls lookin sexy af

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad495670de078dab"; }
+/******/ 		__webpack_require__.h = function() { return "a048cd2a99bf154e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/tour.js
+++ pages/tour.js
@@ -8,6 +8,16 @@ export default function Tour() {
     <div className={styles.content}>
             <a className={styles.back} href='/'>⊲Back to Homepage</a>
             <h1 className={styles.title}>The Tour</h1>
+            <a className={styles.point} href='https://blackcatashland.com/' target='_blank' rel='noopener noreferrer'>Black Cat</a>
+            <a className={styles.point} href='http://www.ashlandbaycinema.com/' target='_blank' rel='noopener noreferrer'>Cinema</a>
+            <a className={styles.point} href='https://www.google.com/maps/place/Houghton+Point/@46.6985625,-90.8617169,15z/data=!3m1!4b1!4m5!3m4!1s0x52a91a4f033e8c03:0x1b589ab84558e2a8!8m2!3d46.6985487!4d-90.8529621' target='_blank' rel='noopener noreferrer'>houghton</a>
+            <a className={styles.point} href='https://www.google.com/maps/place/Mt+Ashwabay+Ski+and+Recreation+Area/@46.7763496,-90.8931916,13.58z/data=!4m5!3m4!1s0x52a91e86044109f9:0xb011f73ec5926153!8m2!3d46.7772179!4d-90.8945541' target='_blank' rel='noopener noreferrer'>ashwabay</a>
         </div>
   );
 }
+
+// Black Cat        https://blackcatashland.com/
+// Ashland Cinema   http://www.ashlandbaycinema.com/
+// Houghton Point   https://www.google.com/maps/place/Houghton+Point/@46.6985625,-90.8617169,15z/data=!3m1!4b1!4m5!3m4!1s0x52a91a4f033e8c03:0x1b589ab84558e2a8!8m2!3d46.6985487!4d-90.8529621
+// Ashwabay         https://www.google.com/maps/place/Mt+Ashwabay+Ski+and+Recreation+Area/@46.7763496,-90.8931916,13.58z/data=!4m5!3m4!1s0x52a91e86044109f9:0xb011f73ec5926153!8m2!3d46.7772179!4d-90.8945541
+// 
