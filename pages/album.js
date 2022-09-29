90f25123dea33345a608512368794acfd390bea1
bpaulsen0 <benjaminpaulsen0@gmail.com>
2022-09-28 19:15:55 -0500

    added timer and new pictures to the album

--- pages/album.js
+++ pages/album.js
@@ -13,7 +13,7 @@ export default function Album() {
             <a className={styles.back} href='/'>⊲Back to Homepage</a>
             <h1 className={styles.title}>Our Family</h1>
             <div className={styles.masonry}>
-                <img className={styles.item} src="/album/benlaurakids.jpg"></img>
+                {/* <img className={styles.item} src="/album/benlaurakids.jpg"></img>
                 <img className={styles.item} src="/album/bikes.jpg"></img>
                 <img className={styles.item} src="/album/boat.jpg"></img>
                 <img className={styles.item} src="/album/boatkatka.jpg"></img>
@@ -46,6 +46,94 @@ export default function Album() {
                 <img className={styles.item} src="/album/zorro_presents.jpg"></img>
                 <img className={styles.item} src="/album/zorro_window.jpg"></img>
                 <img className={styles.item} src="/album/Zorro.jpg"></img>
+                <img className={styles.item} src="/album/20131229_111021 (1).jepg"></img>
+                <img className={styles.item} src="/album/Andy Family.jepg"></img>
+                <img className={styles.item} src="/album/Ben Tia Sail.jpeg"></img>
+                <img className={styles.item} src="/album/BenToys.jpeg"></img>
+                <img className={styles.item} src="/album/BP-6126-2-(ZF-9837-80013-2-011).jpeg"></img>
+                <img className={styles.item} src="/album/BP-6162-(ZF-9837-80013-2-005).jpeg"></img>
+                <img className={styles.item} src="/album/Camping with Cousins1.jpeg"></img>
+                <img className={styles.item} src="/album/DSC07072.jpeg"></img>
+                <img className={styles.item} src="/album/IMG_0508 (1).jpeg"></img>
+                <img className={styles.item} src="/album/IMG_0961.jpeg"></img>
+                <img className={styles.item} src="/album/IMG_20180523_191533088.jpeg"></img>
+                <img className={styles.item} src="/album/IMG_20180627_175641401_HDR.jpeg"></img>
+                <img className={styles.item} src="/album/Jeep.jpeg"></img>
+                <img className={styles.item} src="/album/Lapointe Swim.jpeg"></img>
+                <img className={styles.item} src="/album/Leaves.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 086.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0144.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0472.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0894.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0927.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1492.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1726.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1796.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1877.jpeg"></img>
+                <img className={styles.item} src="/album/Sean Wedding Goofy Pic.jpeg"></img>
+                <img className={styles.item} src="/album/Summer 2010 Cousins.jpeg"></img>
+                <img className={styles.item} src="/album/Summer2010.jpeg"></img> */}
+
+<img className={styles.item} src="/album/eng1.jpg"></img>
+                <img className={styles.item} src="/album/littlewiezoreks.jpg"></img>
+                <img className={styles.item} src="/album/IMG_0961.jpeg"></img>
+                <img className={styles.item} src="/album/littletia2.jpg"></img>
+                <img className={styles.item} src="/album/Me_and_Tia.jpg"></img>
+                <img className={styles.item} src="/album/tia-and-athena.jpg"></img>
+                <img className={styles.item} src="/album/Jeep.jpeg"></img>
+                <img className={styles.item} src="/album/Sean Wedding Goofy Pic.jpeg"></img>
+                <img className={styles.item} src="/album/fam2.jpg"></img>
+<img className={styles.item} src="/album/benlaurakids.jpg"></img>
+                <img className={styles.item} src="/album/zorro_presents.jpg"></img>
+                <img className={styles.item} src="/album/sweeties.jpg"></img>
+                <img className={styles.item} src="/album/zorro_window.jpg"></img>
+                <img className={styles.item} src="/album/20131229_111021 (1).jepg"></img>
+                <img className={styles.item} src="/album/cutetia.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1796.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1726.jpeg"></img>
+                <img className={styles.item} src="/album/boatkatka.jpg"></img>
+                <img className={styles.item} src="/album/tiagrad.jpg"></img>
+                <img className={styles.item} src="/album/IMG_20180523_191533088.jpeg"></img>
+                <img className={styles.item} src="/album/bikes.jpg"></img>
+                <img className={styles.item} src="/album/Camping with Cousins1.jpeg"></img>
+                <img className={styles.item} src="/album/Summer 2010 Cousins.jpeg"></img>
+                <img className={styles.item} src="/album/Wiezorek_Family.jpg"></img>
+                <img className={styles.item} src="/album/meske.jpg"></img>
+                <img className={styles.item} src="/album/Andy Family.jepg"></img>
+                <img className={styles.item} src="/album/Leaves.jpeg"></img>
+                <img className={styles.item} src="/album/Summer2010.jpeg"></img>
+                <img className={styles.item} src="/album/BenToys.jpeg"></img>
+                <img className={styles.item} src="/album/BP-6162-(ZF-9837-80013-2-005).jpeg"></img>
+                <img className={styles.item} src="/album/IMG_0508 (1).jpeg"></img>
+                <img className={styles.item} src="/album/fam1.jpg"></img>
+                <img className={styles.item} src="/album/snowboard.jpg"></img>
+                <img className={styles.item} src="/album/Ben Tia Sail.jpeg"></img>
+                <img className={styles.item} src="/album/wichmans.jpg"></img>
+                <img className={styles.item} src="/album/wiezoreks2.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0472.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0144.jpeg"></img>
+                <img className={styles.item} src="/album/prom.jpg"></img>
+                <img className={styles.item} src="/album/lawnmower.jpg"></img>
+                <img className={styles.item} src="/album/Tianna.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0927.jpeg"></img>
+                <img className={styles.item} src="/album/wiezoreks.jpg"></img>
+                <img className={styles.item} src="/album/boat.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 0894.jpeg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1492.jpeg"></img>
+                <img className={styles.item} src="/album/paulsens.jpg"></img>
+                <img className={styles.item} src="/album/pigroast.jpeg"></img>
+                <img className={styles.item} src="/album/IMG_20180627_175641401_HDR.jpeg"></img>
+                <img className={styles.item} src="/album/waterslide.jpg"></img>
+                <img className={styles.item} src="/album/wiezoreks3.jpg"></img>
+                <img className={styles.item} src="/album/easter.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 086.jpeg"></img>
+                <img className={styles.item} src="/album/BP-6126-2-(ZF-9837-80013-2-011).jpeg"></img>
+                <img className={styles.item} src="/album/DSC07072.jpeg"></img>
+                <img className={styles.item} src="/album/Zorro.jpg"></img>
+                <img className={styles.item} src="/album/Photo Library - 1877.jpeg"></img>
+                <img className={styles.item} src="/album/eng2.jpg"></img>
+                <img className={styles.item} src="/album/cherylchristmas.jpg"></img>
+                <img className={styles.item} src="/album/Lapointe Swim.jpeg"></img>
             </div>
             <footer className={styles.footer}><a className={styles.footerURL}>Thank you for being an amazing part of this adventure!</a></footer>
         </div>
